8638d2dc6961dcc34e222836e396ee658cf0d204
Oleg Reznikov <dragonmale@aol.com>
2011-07-03 23:40:11 +1100

    откат core/urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,17 +1,11 @@
+# -*- coding: utf-8 -*-
 
 from django.conf.urls.defaults import *
-from django.conf import settings
-
-from django.contrib import admin
-admin.autodiscover()
+from core.views import *
 
 urlpatterns = patterns('',
-    (r'^admin/', include(admin.site.urls)),
-    (r'^', include('core.urls')),
+    url(r'^$', index),
+    url(r'^upload', upload),
+    url(r'^lock', lock),
 )
-
-if settings.DEBUG:
-    urlpatterns += patterns('',
-        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT, 'show_indexes': False}),
-     )
 
