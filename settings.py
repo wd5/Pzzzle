221b9895538de0468faca28faadb29147261856a
Mikhail Polykovskij <glader@yandex-team.ru>
2011-08-10 15:26:53 +0600

    404 page

--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@
 import os
 from datetime import timedelta
 
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
@@ -70,7 +70,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = 'pzzzle2.urls'
+ROOT_URLCONF = 'urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
